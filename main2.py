74a52ce11a535ee429a5e2a02e37d9eb9dbba76f
joesrwt <huang.sorawit@gmail.com>
2025-04-14 04:51:08 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -181,8 +181,8 @@ if st.session_state.data_processed:
         y='Area',
         color=alt.Color('Metric:N', scale=alt.Scale(domain=['Convex Area (Rolling Avg)', 'Concave Area (Rolling Avg)'], range=['green', 'blue']))
     ).properties(
-        width=400,
-        height=800
+        width=900,
+        height=500
     )
     rule = alt.Chart(pd.DataFrame({'Frame': [current_frame]})).mark_rule(color='red').encode(x='Frame')
 
