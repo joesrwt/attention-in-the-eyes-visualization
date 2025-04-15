5379921fed747643c9fccb596ecfd7e8b39cd470
joesrwt <huang.sorawit@gmail.com>
2025-04-15 17:05:13 +0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -41,7 +41,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.image(
         "https://raw.githubusercontent.com/nutteerabn/InfoVisual/main/gif_sample/convex_concave_image.jpg",
-        caption="📊 Diagram: Convex vs Concave Hulls"
+        caption="📊 Diagram: Convex vs Concave Hulls",width =300
     )
 with col2:
     st.image(
